c955f7571118ee267d226f169adfb6b72e9a8ebd
Capsy <71144025+Capsy001@users.noreply.github.com>
2023-03-21 22:17:55 +0530

    firestore working

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button
+} from 'react-native';
+import { db, app } from './firebaseconfig';
+import { doc, setDoc } from "firebase/firestore"; 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+      onPress={async ()=>{
+        await setDoc(doc(db, "cities", "LA"), {
+          name: "Los Angeles",
+          state: "CA",
+          country: "USA"
+        });
+        
+    }}
+      title="Click"></Button>
       <StatusBar style="auto" />
     </View>
   );
