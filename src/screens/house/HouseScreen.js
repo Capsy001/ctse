3f3035a546c12a466c955b3687ed7b58dda65535
Chamuditha Prabhath Jayasena <36603594+chamuditha4@users.noreply.github.com>
2023-03-23 11:15:06 +0530

    Update HouseScreen.js

--- src/screens/house/HouseScreen.js
+++ src/screens/house/HouseScreen.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const HouseScreen = () => {
-    return <View></View>
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.component}>
+        <Text>Hello</Text>
+      </View>
+      <View style={styles.component}>
+        <Text>World</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  component: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+  },
+});
 
 export default HouseScreen;
