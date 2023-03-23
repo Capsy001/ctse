53e3d8150464390e29ca04e2dd736ebaa01cfd21
Capsy <71144025+Capsy001@users.noreply.github.com>
2023-03-23 12:57:42 +0530

    Update RoomService.js

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -1,4 +1,7 @@
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { db } from "../../firebaseconfig";
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 export const uploadImage = async (image) => {
 
@@ -9,6 +12,16 @@ export const uploadImage = async (image) => {
     const storage = getStorage()
     const storageRef = ref(storage, `images/${filename}`);
     console.log('Image uploaded to Firebase Storage successfully!');
+
+        // Add a new document in collection "cities"
+await setDoc(doc(db, "cities", "LA"), {
+    name: "llllllllllllllllllllllll",
+    state: "CA",
+    country: "USA"
+  });
+
     return await uploadBytes(storageRef, blob).then(item => getDownloadURL(item.ref));
+
+
 
 }
