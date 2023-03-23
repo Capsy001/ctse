0514744c19434da89d5f4f91203f36c1c8c0580c
Chamuditha Prabhath Jayasena <36603594+chamuditha4@users.noreply.github.com>
2023-03-23 11:34:26 +0530

    Update HouseScreen.js

--- src/screens/house/HouseScreen.js
+++ src/screens/house/HouseScreen.js
@@ -1,14 +1,35 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { firebase, auth } from "../../../firebaseconfig";
+import { MenuButton } from "../../components";
 
 const HouseScreen = () => {
+    const navigation = useNavigation();
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const returnHome = () => {
+    navigation.replace("Home");
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.component}>
-        <Text>Hello</Text>
+        <TouchableOpacity onPress={returnHome} style={styles.buttonReturnHome}>
+          <Text style={styles.buttonText}>Return Home</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.component}>
-        <Text>World</Text>
+        <TouchableOpacity onPress={handleSignOut} style={styles.buttonSignOut}>
+          <Text style={styles.buttonText}>Sign out</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -21,11 +42,74 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-around",
   },
-  component: {
-    backgroundColor: "white",
-    borderRadius: 10,
+  innerContainer: {
+    alignItems: "center",
+    flexDirection: "column",
+    marginLeft: 45,
+  },
+
+  itemHeading: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginRight: 22,
+  },
+  formContainer: {
+    flexDirection: "row",
+    height: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 100,
+  },
+  buttonSignOut: {
+    backgroundColor: "#F03729",
+    width: "100%",
     padding: 10,
-    margin: 10,
+    borderRadius: 0,
+    // marginTop: 100,
+    // marginBottom:-90,
+    alignItems: "center",
+  },
+  buttonReturnHome: {
+    backgroundColor: "#BD2BF7",
+    width: "100%",
+    padding: 10,
+    borderRadius: 0,
+    // marginTop: 100,
+    // marginBottom:-90,
+    alignItems: "center",
+  },
+
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "#E7C1F7",
+    paddingLeft: 16,
+    flex: 1,
+    marginRight: 5,
+  },
+  button: {
+    height: 47,
+    borderRadius: 5,
+    backgroundColor: "#BD2BF7",
+    width: 80,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  },
+
+  todoIcon: {
+    marginTop: 10,
+    fontSize: 20,
+    marginLeft: 14,
+  },
+  menus: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
 });
 
