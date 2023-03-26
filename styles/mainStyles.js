431f61b95aa8519acece6d811370cd3612a28286
Gayesha Upeshika <gayeshaupeshika11@gmail.com>
2023-03-26 07:14:31 +0530

    new
    new

---
+++ styles/mainStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const mainStyles = StyleSheet.create({
+    centerPage: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default mainStyles;
